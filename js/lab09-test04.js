2bafc45b21f22f332d32ba5b21790b31c4a76083
danvynguyen <danvynguyen876@gmail.com>
2022-11-23 13:57:31 -0700

    commit 2

--- js/lab09-test04.js
+++ js/lab09-test04.js
@@ -27,37 +27,45 @@ document.addEventListener('DOMContentLoaded', function () {
 	let sliderOpacity = document.getElementById("sliderOpacity");
 	sliderOpacity.addEventListener('click', function () {
 		let opacity = Number(sliderOpacity.getAttribute("value"));
-		largeImage.style.opacity=opacity/100;
+		console.log(opacity);
+		largeImage.style.filter="opacity("+opacity/100+")";
+		let num = document.getElementById("numOpacity");
+		num.innerHTML = opacity;
 	});
 	
 	let sliderSaturation = document.getElementById("sliderSaturation");
 	sliderSaturation.addEventListener('click', function () {
 		let saturation = Number(sliderSaturation.getAttribute("value"));
+		console.log(saturation);
 		largeImage.style.filter="saturate("+(saturation/300)*100+"%)";
 	});
 	
 	let sliderBrightness = document.getElementById("sliderBrightness");
 	sliderBrightness.addEventListener('click', function () {
 		let brightness = Number(sliderBrightness.getAttribute("value"));
+		console.log(brightness);
 		largeImage.style.filter="brightness("+brightness/300+")";
 	});
 	
 	let sliderHue = document.getElementById("sliderHue");
 	sliderHue.addEventListener('click', function () {
 		let hue = Number(sliderHue.getAttribute("value"));
+		console.log(hue);
 		largeImage.style.filter="hue-rotate("+(hue/360)*100+"deg)";
 	});
 	
 	let sliderGray = document.getElementById("sliderGray");
 	sliderGray.addEventListener('click', function () {
 		let grayscale = Number(sliderGray.getAttribute("value"));
+		console.log(grayscale);
 		largeImage.style.filter="grayscale("+(grayscale/100)*100+"%)";
 	});
 	
 	let sliderBlur = document.getElementById("sliderBlur");
 	sliderBlur.addEventListener('click', function () {
 		let blur = Number(sliderBlur.getAttribute("value"));
-		largeImage.style.filter="grayscale("+(blur/100)*100+"px)";
+		console.log(blur);
+		largeImage.style.filter="blur("+(blur/100)*100+"px)";
 	});
 	
 	let resetButton = document.getElementById("resetFilters");
